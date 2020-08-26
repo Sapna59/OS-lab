4b8fbb3b4cf6b847a5a9cb73eb2f573964ca766b
Sapna59 <48959730+Sapna59@users.noreply.github.com>
2020-08-26 15:13:04 +0530

    Add files via upload
    Assignment 3 ,orphan and zombie process, multiprocessing

---
+++ 3a.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<unistd.h>
+ 
+int main()
+{
+    pid_t pid;
+    char *message;
+    int n,exit_code;
+    printf("Fork program starting\n");
+ 
+    pid = fork();
+ 
+    switch(pid)
+    {
+        case -1:
+            perror("Fork failed\n");
+            exit(1);
+        case 0:
+            message = "This is the child\n";
+            n = 2;
+            break;
+        default:
+            message = "This is the parent\n";
+            n = 5;
+            break;
+    }
+    for( ;n > 0; n--)
+    {
+        puts(message);
+        printf("pid=%d,parent=%d\n",getpid(),getppid());
+        sleep(1);
+ 
+    }
+    exit(0);
+}
